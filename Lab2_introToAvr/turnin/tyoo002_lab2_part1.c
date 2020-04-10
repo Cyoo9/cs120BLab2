ba55eee0e171b357acec7eced0693bb6300878ce
Taeho Yoo <tyoo002@wch136-09.cs.ucr.edu>
2020-04-09 22:26:07 -0700

    fixed part 1

--- Lab2_introToAvr/turnin/tyoo002_lab2_part1.c
+++ Lab2_introToAvr/turnin/tyoo002_lab2_part1.c
@@ -16,32 +16,22 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB= 0x00;
-    //DDRC = 0xFF; PORTC = 0x00;
-     
-    //unsigned char cntavail = 0x04;
    
-    unsigned char tempB = 0x00; //LED light temp var
-    unsigned char tempA = 0x00; //input 
+   
+    unsigned char tempB; //LED light temp var
+    unsigned char tempA; //input 
 
     while (1) {
+	tempB = 0x00;
 	tempA = PINA & 0x03;
-	//cntavail = 0x04;
 
 	if(tempA == 0x01) {
 		tempB = 0x01;
 	}
 	
-	/*if(PINA & 0x01) { cntavail--; }
-	if(PINA & 0x02) { cntavail--; }
-	if(PINA & 0x04) { cntavail--; }
-	if(PINA & 0x08) { cntavail--; }
-	
-	if(cntavail == 0) { PORTC = PORTC | 0x8F; }
-	*/
     	PORTB = tempB; //LED port (ex 1)
 	
 
-	//PORTC = cntavail; //available spaces (ex 2)
     }
     return 0;
 }
